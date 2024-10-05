3651d9038d1c464385a7b7c285a836c7a9bf5167
Juan Carlos <jcromero909@misena.edu.co>
2024-10-05 12:26:53 -0500

    feat: Add lab1 nextjs project with 2 different routes

---
+++ lab1/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div className="m-auto max-w-2xl mt-4 flex flex-col gap-5">
+            <h1 className="text-xl text-center font-bold">About</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe labore nam earum consequuntur quae repellendus dolore architecto accusamus beatae veniam exercitationem aut ratione recusandae consectetur quisquam, libero nesciunt quas? Maxime.📱
+            </p>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus rerum, iste quisquam odit expedita quae incidunt facilis reprehenderit illo similique dignissimos dolorem provident aut, quo, saepe veritatis vel animi voluptatem!
+            </p>
+        </div>
+    );
+}
---
+++ lab1/src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className="m-auto max-w-2xl mt-4">
+            <h1 className="text-xl text-center font-bold">Contact</h1>
+            <p>This is a contact page 📱</p>
+        </div>
+    );
+}
